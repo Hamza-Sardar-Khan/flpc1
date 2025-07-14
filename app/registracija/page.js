92e6b7756e1b7a3f4cc6e8a174400fafde840112
Hamza-Sardar-Khan <naveedabdullah800@gmail.com>
2025-07-15 00:18:09 +0500

    Header Changes

--- app/registracija/page.js
+++ app/registracija/page.js
@@ -2,13 +2,18 @@
 
 import Registration from "@/app/components/appcomponents/Registration";
 import HeaderRegistration from "@/app/components/appcomponents/HeaderRegistration";
+import Layout from "../components/appcomponents/Layout";
 
 const RegistrationPage = () => {
   return (
-    <div className="flex flex-1 flex-col bg-[#F5F7F9]">
-      <HeaderRegistration />
-      <Registration />
-    </div>
+    <Layout from={"18"} forFooter={"memorypage"} currentPage="">
+
+      <div className="flex flex-1 flex-col bg-[#F5F7F9]">
+        {/* <HeaderRegistration /> */}
+        <Registration />
+      </div>
+
+    </Layout>
   );
 };
 
